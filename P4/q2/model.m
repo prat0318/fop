41c59142d1a95ec8e34d9582ece72ad719b72162
Gaurav Nanda <gnanda@TACC-AirSpare02.local>
2013-10-02 03:17:39 -0500

    model

--- P4/q2/model.m
+++ P4/q2/model.m
@@ -1,11 +1,15 @@
-Pr : [counter] [sizeofDel] [sizeOfBasic] [sizeOf] [debug] [rootDel] [delete] root :: PL ;
+Pr : [counterBasic] [sizeofDel] [sizeOfBasic] [debugBasic] [rootDel] [delete] [sizeOf] [counter] [debug] root :: PL ;
 
 %%
+debugBasic iff (debug);
+counterBasic iff (counter);
 rootDel iff (root and delete);
 sizeOfBasic iff (sizeOf);
 sizeofDel iff (sizeOf and delete);
 ##
 rootDel {hidden}
+counterBasic {hidden}
+debugBasic {hidden}
 sizeOfBasic {hidden}
 sizeofDel  {hidden}
 delete  {out=""}
