01901d67a7c443d5d7bddbaa40614a2fbe650205
Prateek Agarwal <prat0318@gmail.com>
2013-11-21 23:33:39 -0600

    transfer tests

--- P9/ParallelSort/src/hashJoin/MainTest.java
+++ P9/ParallelSort/src/hashJoin/MainTest.java
@@ -25,49 +25,15 @@ import java.util.TreeMap;
  */
 public class MainTest {
     public static void main(String[] args) throws Exception {
-        readRelation_PrintTupleTest("client.txt");
-        readRelation_PrintTupleTest("viewing.txt");
         readRelation_Hsplit_PrintTuple();
-        readRelation_Hjoin_PrintTupleTest("client.txt","viewing.txt");
         gammaTest("client.txt","viewing.txt");
         readRelation_Hsplit_Merge_PrintTuple();
-        bloomTest();
         mapReduceBloomTest();
         testBloomSimulator();
         testBFilterAndPrint();
         testBloomSimulatorFilter();
     }
 
-    public static void readRelation_PrintTupleTest(String fileName) {
-        // read --> sort --> print
-        System.out.println("Starting ReadRelation _ PrintTuple");
-        Connector read_A = new Connector("input1");
-        ReadRelation r = new ReadRelation(fileName,"clientDB", read_A.getWriteEnd());
-        PrintTuple p = new PrintTuple(read_A.getReadEnd());
-        r.start();
-        p.start();
-        System.out.println("-----------------");
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-    
-    public static void bloomTest() throws Exception {
-        System.out.println("Starting Bloom Test");
-        Connector print_bloom = new Connector("print_bloom");
-        ReadRelation r = new ReadRelation("client.txt","clientDB", print_bloom.getWriteEnd());
-        Connector bloom_join = new Connector("bloom_join");
-        PrintTuple p1 = new PrintTuple(bloom_join.getReadEnd());
-        Connector bloom_bmap = new Connector("bloom_bmap");
-        ReadEnd readEnd = bloom_bmap.getReadEnd();
-        Bloom bloom = new Bloom(print_bloom.getReadEnd(), bloom_join.getWriteEnd(), bloom_bmap.getWriteEnd(), 0);
-        r.start();
-        bloom.start();
-        p1.start();       
-        System.out.println(readEnd.getNextString());
-    }
 
     public static void mapReduceBloomTest() throws Exception {
         System.out.println("Starting Map Reduce Bloom Test");
@@ -126,32 +92,6 @@ public class MainTest {
         }        
     }
     
-    public static void readRelation_Hjoin_PrintTupleTest(String file1, String file2) {
-        //System.out.println("Starting ReadRelation _ HJoin _ PrintTuple");
-        Connector read_A = new Connector("input1");
-        ReadRelation r1 = new ReadRelation(file1,file1, read_A.getWriteEnd());
-        Connector read_B = new Connector("input2");
-        ReadRelation r2 = new ReadRelation(file2,file2, read_B.getWriteEnd());
-        Connector out = new Connector("output");
-        HJoin h = new HJoin(0, read_A.getReadEnd(),0,read_B.getReadEnd(),out.getWriteEnd());
-        PrintTuple p = new PrintTuple(out.getReadEnd());
-        r1.start();
-        r2.start();
-        h.start();
-        p.start();
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-//        Connector out2 = new Connector("output2");
-//        MapReduceHJoin mapJ = new MapReduceHJoin(0, file1, 0 , file2,out2.getWriteEnd() );
-//        mapJ.start();
-//        PrintTuple p2 = new PrintTuple(out2.getReadEnd());
-//        p2.start();
-
-    }
     public static void readRelation_Hsplit_Merge_PrintTuple() {
         System.out.println("Starting ReadRelation _ Hsplit _ Merge _ PrintTuple");
         Connector read_A = new Connector("input1");
--- P9/ParallelSort/test/Test1.java
+++ P9/ParallelSort/test/Test1.java
@@ -5,6 +5,8 @@
 
 import gammaContainers.HJoinWithBloomFilter;
 import gammaContainers.MapReduceHJoin;
+import hashJoin.Bloom;
+import hashJoin.HJoin;
 import hashJoin.PrintTuple;
 import hashJoin.basicConnector.Connector;
 import org.junit.After;
@@ -14,6 +16,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import hashJoin.MainTest;
+import hashJoin.ReadRelation;
+import hashJoin.basicConnector.ReadEnd;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -46,12 +50,68 @@ public class Test1 {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
+    
+    @Test
+    public void testReadRelation_PrintTuple() {
+        RegTest.Utility.redirectStdOut("out.txt");  // redirects standard out to file "out.txt"
+        System.out.println("Starting ReadRelation _ PrintTuple");
+        Connector read_A = new Connector("input1");
+        ReadRelation r = new ReadRelation("client.txt","clientDB", read_A.getWriteEnd());
+        PrintTuple p = new PrintTuple(read_A.getReadEnd());
+        r.start();
+        p.start();
+        System.out.println("-----------------");
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        MainTest.FileSort("out.txt");
+        RegTest.Utility.validate("out.txt", "readRelationPrint.txt", false); // test passes if files are equal
+    }    
+
+    @Test       
+    public void testBloom() throws Exception {
+        System.out.println("Starting Bloom Test");
+        Connector print_bloom = new Connector("print_bloom");
+        ReadRelation r = new ReadRelation("client.txt","clientDB", print_bloom.getWriteEnd());
+        Connector bloom_join = new Connector("bloom_join");
+        PrintTuple p1 = new PrintTuple(bloom_join.getReadEnd());
+        Connector bloom_bmap = new Connector("bloom_bmap");
+        ReadEnd readEnd = bloom_bmap.getReadEnd();
+        Bloom bloom = new Bloom(print_bloom.getReadEnd(), bloom_join.getWriteEnd(), bloom_bmap.getWriteEnd(), 0);
+        r.start();
+        bloom.start();
+        p1.start();    
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assert("fftffftfffffffftfffffftfffff".equals(readEnd.getNextString())); 
+    }
+    
     @Test
     public void testJoin() {
         RegTest.Utility.redirectStdOut("out.txt");  // redirects standard out to file "out.txt"
 
         String[] args = null;
-        MainTest.readRelation_Hjoin_PrintTupleTest("client.txt", "viewing.txt");
+        Connector read_A = new Connector("input1");
+        ReadRelation r1 = new ReadRelation("client.txt", "client.txt", read_A.getWriteEnd());
+        Connector read_B = new Connector("input2");
+        ReadRelation r2 = new ReadRelation("viewing.txt", "viewing.txt", read_B.getWriteEnd());
+        Connector out = new Connector("output");
+        HJoin h = new HJoin(0, read_A.getReadEnd(),0,read_B.getReadEnd(),out.getWriteEnd());
+        PrintTuple p = new PrintTuple(out.getReadEnd());
+        r1.start();
+        r2.start();
+        h.start();
+        p.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         MainTest.FileSort("out.txt");
         RegTest.Utility.validate("out.txt", "ClientJoinViewing.txt", false); // test passes if files are equal
     }
