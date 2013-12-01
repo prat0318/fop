8338272e7829d5e0e9536583ef28e075fe466311
bansal <akankshabansal90@gmail.com>
2013-12-01 17:27:56 -0600

    constraints check

--- argouml/constraintsCheck.pl
+++ argouml/constraintsCheck.pl
@@ -1,5 +1,42 @@
 /* uniqueNames CONSTRAINT: Class, and Interfaces have unique names constraint */
 
+dbase(argo,[class,interface,association,composition,attribute]).
+
+table(class,[id,"name","visibility",super_id]).
+table(interface,[id,"name","visibility",super_id]).
+table(attribute,[atr_id,"atr_name","atr_visibility","class_id"]).
+table(association,[end,class_id,multiplicity]).
+
+interface(i_1, abd, adc,iabd ).
+class(class_1, shoppingCart, vk_public, class_2).
+attribute(attr_1, subTotalMoney, vk_public).
+attribute(attr_2, vatAmount, vk_public).
+attribute(attr_3, totalMoeny, vk_public).
+
+class(class_2, customer, vk_public, class_3).
+attribute(attr_4, customerName, vk_public).
+attribute(attr_5, emailAddress, vk_private).
+
+class(class_3, creditCard, vk_public, class_1).
+attribute(attr_6, issuer, vk_public).
+attribute(attr_7, cardNumber, vk_private).
+
+class(class_4, itermToPurchase, vk_public, class_1).
+attribute(attr_8, quantity, vk_public).
+attribute(attr_9, pricePerUnit, vk_public).
+
+composition(assoc_1, customer_Cart).
+association(assoc_end_1, class_1, "0..inf").
+association(assoc_end_2, class_2, "1..1").
+
+composition(assoc_2, cart_Item).
+association(assoc_end_3, class_1, "1..1").
+association(assoc_end_4, class_4, "1..inf").
+
+composition(assoc_3, cart_Card).
+association(assoc_end_5, class_1, "1..1").
+association(assoc_end_6, class_3, "1..1").
+
 findPair(I1,I2,N):-ename(I1,N),ename(I2,N),@I1 @< @I2.
 uniqueNames:-forall(findPair(_,_,N),isError('unique names constraint violated: ',N)).
 
@@ -7,12 +44,12 @@ uniqueNames:-forall(findPair(_,_,N),isError('unique names constraint violated: '
 /* circular CONSTRAINT: Check circularity of class Hierarchy */
 
 /*  utility predicates */
-isClass(I,N):-argoClass(I,N,_,_,_,_,_).
-isIntf(I,N):-argoInterface(I,N,_,_,_).
+isClass(I,N):-class(I,N,_,_).
+isIntf(I,N):-interface(I,N,_,_).
 ename(I,N):-isIntf(I,N);isClass(I,N).
 
-subClass(Sub,Sup):-argoAssociation(Sup,_,'TRIANGLE',_,Sub,_,_,_,_),isClass(Sup,_),isClass(Sub,_).
-subClass(Sub,Sup):-argoAssociation(Sub,_,_,_,Sup,_,'TRIANGLE',_,_),isClass(Sup,_),isClass(Sub,_).
+
+subClass(Sub,Sup):-class(Sub,_,_,Sup),isClass(Sup,_),isClass(Sub,_).
 subOf(Sub,Sup):-subClass(Sub,Sup).
 subOf(Sub,Sup):-subClass(Sub,X),subOf(X,Sup).
 
@@ -23,8 +60,7 @@ circular:-forall(isClass(I,N),testCycle(I,N)).
 
 /* icircular CONSTRAINT: Check circularity of inheritance Hierarchy  */
 
-subIntf(Sub,Sup):-argoAssociation(Sup,_,'TRIANGLE',_,Sub,_,_,_,_),isIntf(Sup,_),isIntf(Sub,_).
-subIntf(Sub,Sup):-argoAssociation(Sup,_,'TRIANGLE',_,Sub,_,_,_,_),isIntf(Sup,_),isIntf(Sub,_).
+subIntf(Sub,Sup):-interface(Sub,_,_,Sup),isIntf(Sup,_),isIntf(Sub,_).
 isubOf(Sub,Sup):-subIntf(Sub,Sup).
 isubOf(Sub,Sup):-subIntf(Sub,X),isubOf(X,Sup).
 
@@ -33,22 +69,6 @@ testICycle(_,_).
 
 icircular:-forall(isIntf(I,N),testICycle(I,N)).
 
-/* noDoubleEnds CONSTRAINT: Check that no edge has diamond, arrow, triangle, etc on both sides */
-
-hasEndShape('BLACK_DIAMOND').
-hasEndShape('TRIANGLE').
-%hasEndShape('V').
-hasEndShape('DIAMOND').
-
-testHasDoubleEnds(I,N):- argoAssociation(I,_,Arrow1,_,ToNodeId,_,Arrow2,_,_),
-                            hasEndShape(Arrow1), hasEndShape(Arrow2),
-                                argoClass(ToNodeId,ToNodeName,_,_,_,_,_),
-                                    isError('double ended edge: ',[N,ToNodeName]).
-testHasDoubleEnds(_,_).
-
-noDoubleEnds :- forall(isClass(I,N),testHasDoubleEnds(I,N)).
-inoDoubleEnds :- forall(isIntf(I,N),testHasDoubleEnds(I,N)).
-
 /* legalMultiplicity CONSTRAINT: Check the legality of the multiplicity */
 
 noMult('').
@@ -56,7 +76,7 @@ noMult('').
 isTriangle('TRIANGLE').
 
 %(\+ noMult(Mult1);\+ noMult(Mult2)),
-testMultiplicity(I,N):- argoAssociation(I,Mult1,Arrow1,_,_,Mult2,Arrow2,_,_),
+testMultiplicity(I,N):- association(I,Mult1,Arrow1,_,_,Mult2,Arrow2,_,_),
                             (not(noMult(Mult1));not(noMult(Mult2))),
                                 (isTriangle(Arrow1);isTriangle(Arrow2)), 
                                     isError('illegal multiplicity: ',N).
@@ -66,17 +86,8 @@ legalMultiplicity :- forall(isClass(I,N),testMultiplicity(I,N)).
 ilegalMultiplicity :- forall(isIntf(I,N),testMultiplicity(I,N)).
 
 
-/* position CONSTRAINT: every class and interface has a position */
-
-testPosition(I):-not(position(I,_,_)),isError('class has no position',I).
-testPosition(_).
-
-hasPosition:-forall(ename(I,_),testPosition(I)).
-
-
 /* ALL CONSTRAINTS */
 
-run:-uniqueNames,circular,icircular,hasPosition,
-        noDoubleEnds,inoDoubleEnds,
+run:-uniqueNames,circular,icircular,
             legalMultiplicity,ilegalMultiplicity.
 
