1067d991a19606e78a639476cd60e980bd783d24
bansal <akankshabansal90@gmail.com>
2013-12-07 18:26:08 -0600

    Constraints Check

--- argouml/discontiguous.pl
+++ argouml/discontiguous.pl
@@ -1,3 +1,3 @@
 /** file   discontiguous.pl -- eliminates aggrevating warnings*   about tuples of a table not being listed sequentially **/
 
-:- discontiguous table/1, composition/2, class/4, attribute/3, association/3.
+:- discontiguous table/2, composition/2, class/4, attribute/4, association/4, interface/4, association_end/4.
