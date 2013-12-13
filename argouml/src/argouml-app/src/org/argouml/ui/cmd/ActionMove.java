9107f3d48033ae78c65ae09c7970b8cb45d2448b
sreesurendran <surendran54>
2013-12-13 12:27:45 -0600

    Move attributes/classes to classes associated with the source

--- argouml/src/argouml-app/src/org/argouml/ui/cmd/ActionMove.java
+++ argouml/src/argouml-app/src/org/argouml/ui/cmd/ActionMove.java
@@ -2,7 +2,10 @@ package org.argouml.ui.cmd;
 
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
@@ -15,8 +18,12 @@ import org.argouml.ui.MoveBox;
 import org.argouml.ui.targetmanager.TargetManager;
 import org.argouml.uml.diagram.ArgoDiagram;
 import org.argouml.uml.diagram.DiagramUtils;
+import org.omg.uml.foundation.core.AssociationEnd;
 import org.omg.uml.foundation.core.Classifier;
+import org.omg.uml.foundation.core.Generalization;
 import org.omg.uml.foundation.core.UmlClass;
+import org.omg.uml.foundation.core.AssociationClass;
+import org.omg.uml.foundation.core.UmlAssociation;
 import org.omg.uml.foundation.core.Operation;
 import org.omg.uml.foundation.core.Attribute;
 import org.tigris.gef.presentation.Fig;
@@ -42,28 +49,40 @@ public class ActionMove extends AbstractAction {
     public void actionPerformed(ActionEvent ae) {
     	ArgoDiagram diagram = DiagramUtils.getActiveDiagram();
     	List nodes = diagram.getNodes();
+    	List edges = diagram.getEdges();
     	List<UmlClass> classes = new ArrayList<UmlClass>();
     	Object source = TargetManager.getInstance().getTarget();
     	
-    	if (!nodes.isEmpty()) {
-    		for(int i=0;i<nodes.size();i++){
-    			UmlClass c = (UmlClass)nodes.get(i);  
+    	    	
+    	if (!edges.isEmpty()) {
+    		for(int i=0;i<edges.size();i++){
+    			UmlClass srcClass = null;
     			if(source instanceof Operation){
-    				if(((Operation) source).getOwner().getName() == c.getName()){
-    					continue;
-    				}
-        			else{
-        				classes.add(c);
-        			}    			    				
-    			}    			
-    			else if(source instanceof Attribute){
-    				if(((Attribute) source).getOwner().getName() == c.getName()){
-        				continue;
-        			}   
-        			else{
-        				classes.add(c);
-        			}    			    				    				
+    				Operation op = (Operation)source;
+    				srcClass = (UmlClass)op.getOwner();
     			}
+    			else if(source instanceof Attribute){
+    				Attribute at = (Attribute)source;
+    				srcClass = (UmlClass)at.getOwner();
+    			}
+    			Map<String,AssociationEnd> aemap = new HashMap<String,AssociationEnd>();
+   				UmlAssociation c = (UmlAssociation)edges.get(i);
+   				LOG.info("@@@@@@@@##############TEST: " + c.getName());
+   				List<AssociationEnd> aend = c.getConnection();
+   				for(AssociationEnd a:aend){
+   					LOG.info("@@@@@@@@##############TEST: " + a.getParticipant().getName());
+   					aemap.put(c.getName()+a.getParticipant().getName(), a);
+   				}
+    			
+   				if(aemap.containsKey(c.getName()+srcClass.getName())){
+   					for (Map.Entry<String, AssociationEnd> entry : aemap.entrySet())
+   					{
+   						if(!entry.getKey().equals(c.getName()+srcClass.getName())){
+   							UmlClass targetClass = (UmlClass)entry.getValue().getParticipant();
+   							classes.add(targetClass);
+   						}
+   					}
+   				}   			
     		}
     		
     		if (source != null && (source instanceof Operation || source instanceof Attribute)) {
